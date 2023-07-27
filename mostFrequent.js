57e0c870c478126ad2272653b28494ddd220171e
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-07-27 17:31:04 +0600

    most frequent

---
+++ mostFrequent.js
@@ -0,0 +1,25 @@
+const numArr = [3, 5, 2, 5, 3, 3, 1, 4, 5, 3]
+
+const mostFrequent = (arr) => {
+
+    const frequencyObj = {}
+
+    arr.forEach(num => frequencyObj[num] = (frequencyObj[num] || 0) + 1)
+    
+    let maxFrequency = 0;
+    let mostFrequentElement;
+
+    for (let element in frequencyObj) {
+        if (frequencyObj[element] > maxFrequency) {
+            maxFrequency = frequencyObj[element];
+            mostFrequentElement = element;
+        }
+    }
+
+    return mostFrequentElement
+
+}
+
+console.log(mostFrequent(numArr));
+
+
